c0ec1086a17bd5396e8213fe44fdc7c3e8680c38
Mabel Calim Costa Mabelete <Mabel@Mabel.local>
2020-05-19 17:40:16 -0300

    doces e favoritas ok

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 import os
 
+
 class ImageButton(ButtonBehavior, Image):
     pass
 class LabelButton(ButtonBehavior, Label):
@@ -111,12 +112,11 @@ class Doces(Screen):
     def load(self, path, filename):
         self.ids['doces_msg'].text = ""
         #path = str(os.path.abspath(os.path.dirname(__file__)))
-        #path = path + '/doces/'
-        path_d = str(os.path.abspath(os.path.dirname(__file__))) + "/doces"
-        print (path_d)
+        #path = path + '/doces/'xs
+        path_d = str(os.path.abspath(os.path.dirname(__file__))) #+ "/doces"
+        #print (path_d)
         with open(os.path.join(path_d, filename[0])) as stream:
             self.text_input.text = stream.read()
-        print(filename[0][33::])
 
 
         #self.dismiss_popup()
@@ -125,11 +125,29 @@ class Doces(Screen):
         #if self.ids['text_input2'].text == '':
         #    self.ids['doces_msg'].text = " Atenção, dê um nome para receita modificada aqui embaixo. Ex: nova_receita.txt"
         #else:
-        with open(os.path.join(path, filename), 'w') as stream:
-            stream.write(filename[0][33::])
+        print (filename)
+        with open(os.path.join(path, filename[0]), 'w') as stream:
+            #print (os.path.join(path, filename[0]))
+            stream.write(self.text_input.text)
         self.ids['doces_msg'].text = "Sua receita foi salva com sucesso!"
 
-        #self.dismiss_popup()
+
+    def fav(self,path, filename):
+        #if self.ids['text_input2'].text == '':
+        #    self.ids['doces_msg'].text = " Atenção, dê um nome para receita modificada aqui embaixo. Ex: nova_receita.txt"
+        #else:
+        pathf = str(os.path.abspath(os.path.dirname(__file__))) + "/favoritas_S2"
+        print (filename)
+        #print (filename[0][-1])
+        nome = filename[0].split("/")[-1]
+        with open(os.path.join(pathf, nome), 'w') as stream:
+            #print (os.path.join(pathf, filename[0]))
+            stream.write(self.text_input.text)
+            print(os.path.join(pathf, nome))
+
+        self.ids['doces_msg'].text = "Sua receita favoritada!"
+
+
 
     def change_scroll_y(self, ti, scrlv):
         y_cursor = ti.cursor_pos[1]
@@ -141,45 +159,54 @@ class Doces(Screen):
             if y_cursor - ti.line_height <= y_bar:
                     dy = (y_cursor - ti.line_height) - y_bar
                     scrlv.scroll_y = scrlv.scroll_y + scrlv.convert_distance_to_scroll(0, dy)[1]
+
 class Favoritas(Screen):
     loadfile = ObjectProperty(None)
     savefile = ObjectProperty(None)
     text_input = ObjectProperty(None)
+    path_d = str(os.path.abspath(os.path.dirname(__file__))) + "/doces"
     path_f = str(os.path.abspath(os.path.dirname(__file__))) + "/favoritas_S2"
-    print (path_f)
 
 
     def show_load(self):
-        self.ids['doces_msg'].text = ""
+        self.ids['fv_msg'].text = ""
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
         self._popup = Popup(title="Load file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
     def show_save(self):
-        self.ids['doces_msg'].text = ""
+        self.ids['fv_msg'].text = ""
         content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
         self._popup = Popup(title="Save file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
     def load(self, path, filename):
-        self.ids['doces_msg'].text = ""
-        path_f = str(os.path.abspath(os.path.dirname(__file__))) + "/favoritas_S2"
-        print (path_f)
+        self.ids['fv_msg'].text = ""
+        path_f = str(os.path.abspath(os.path.dirname(__file__))) #+ "/favoritas_S2"
+        #print (path_f)
         with open(os.path.join(path_f, filename[0])) as stream:
             self.text_input.text = stream.read()
 
     def save(self, path, filename):
-        if self.ids['text_input2'].text == '':
-            self.ids['doces_msg'].text = " Atenção, dê um nome para receita modificada aqui embaixo. Ex: nova_receita.txt"
-        else:
-            with open(os.path.join(path, filename), 'w') as stream:
-                stream.write(self.text_input.text)
-            self.ids['doces_msg'].text = "Sua receita foi salva com sucesso!"
+        print (filename)
+        with open(os.path.join(path, filename[0]), 'w') as stream:
+            #print (os.path.join(path, filename[0]))
+            stream.write(self.text_input.text)
+        self.ids['fv_msg'].text = "Sua receita foi salva com sucesso!"
 
-        #self.dismiss_popup()
 
+    def change_scroll_y(self, ti, scrlv):
+        y_cursor = ti.cursor_pos[1]
+        y_bar = scrlv.scroll_y * (ti.height-scrlv.height)
+        if ti.height > scrlv.height:
+            if y_cursor >= y_bar + scrlv.height:
+                    dy = y_cursor - (y_bar + scrlv.height)
+                    scrlv.scroll_y = scrlv.scroll_y + scrlv.convert_distance_to_scroll(0, dy)[1]
+            if y_cursor - ti.line_height <= y_bar:
+                    dy = (y_cursor - ti.line_height) - y_bar
+                    scrlv.scroll_y = scrlv.scroll_y + scrlv.convert_distance_to_scroll(0, dy)[1]
 
 
 GUI = Builder.load_file("main.kv")  # Make sure this is after all class definitions!
